94a67038519cf01ac435d3dd132f56fc8ec2722e
rgangela99 <rgangela99@gmail.com>
2020-11-30 18:42:07 -0500

    updated implementation to have more intuitive interface

--- convokit/surprise/surprise.py
+++ convokit/surprise/surprise.py
@@ -1,13 +1,8 @@
 import numpy as np
-import pandas as pd
 from collections import defaultdict
 from convokit import Transformer
-from convokit.model import Corpus, CorpusComponent, Conversation, Speaker, Utterance
-from convokit.speaker_convo_helpers import SpeakerConvoAttrs
+from convokit.model import Corpus, CorpusComponent, Utterance
 from itertools import chain
-from math import log2
-from numpy import inf
-from pandas.core.groupby.generic import SeriesGroupBy
 from sklearn.feature_extraction.text import CountVectorizer
 from typing import Callable, List, Tuple, Union
 
@@ -29,7 +24,7 @@ def _cross_entropy(target, context, smooth=True):
   context_log_probs = -np.log(context + k / (N_context + V))
   return np.dot(target / N_target, context_log_probs)
 
-def sample(toks: Union[np.ndarray, List[str]], sample_size: int, n_samples=50):
+def sample(toks: Union[np.ndarray, List[str]], sample_size: int, n_samples=50, p=None):
   """
   Generates random samples from a list of tokens.
 
@@ -41,52 +36,52 @@ def sample(toks: Union[np.ndarray, List[str]], sample_size: int, n_samples=50):
   """
   if len(toks) < sample_size: return None
   rng = np.random.default_rng()
-  return rng.choice(toks, (n_samples, sample_size))
+  return rng.choice(toks, (n_samples, sample_size), p=p)
 
 
 class Surprise(Transformer):
-  df_col_name = {
-    'conversation': 'conversation_id',
-    'speaker': 'speaker',
-    'utterance': 'id'
-  }
-
   """
   Computes how surprising a target is based on some context. The measure for surprise used is cross entropy.
   Uses fixed size samples from target and context text to mitigate effects of length on cross entropy.
 
+  :param model_key_selector: function that defines how utterances should be mapped to models
   :param cv: optional CountVectorizer used to tokenize text and create term document matrices. 
       default: scikit learn's default CountVectorizer
+  :param surprise_attr_name: 
   :param target_sample_size: number of tokens to sample from each target
   :param context_sample_size: number of tokens to sample form each context
   :param n_samples: number of samples to take for each target-context pair
   :param sampling_fn: function for generating samples
+  :param smooth: whether to use laplace smoothing when calculating surprise
   """
-  def __init__(self, cv=CountVectorizer(), target_sample_size=100, context_sample_size=100, n_samples=50, 
-      sampling_fn: Callable[[np.ndarray, int], np.ndarray]=sample, surprise_attr_name="surprise", 
-      verbosity=0):
+  def __init__(self, model_key_selector: Callable[[Utterance], str],
+      cv=CountVectorizer(), 
+      surprise_attr_name="surprise",
+      target_sample_size=100, context_sample_size=100, n_samples=50, 
+      sampling_fn: Callable[[np.ndarray, int], np.ndarray]=sample, 
+      smooth: bool=True):
+    self.model_key_selector = model_key_selector
     self.cv = cv
+    self.surprise_attr_name = surprise_attr_name
     self.target_sample_size = target_sample_size
     self.context_sample_size = context_sample_size
     self.n_samples = n_samples
     self.sampling_fn = sampling_fn
-    self.surprise_attr_name = surprise_attr_name
-    self.verbosity = verbosity
+    self.smooth = smooth
   
-  def fit(self, corpus: Corpus, 
-      group_models_by: List[str]=[]):
+  def fit(self, corpus: Corpus,
+      model_text_selector: Callable[[Utterance], List[str]]):
     """
     Fit CountVectorizers to utterances in a corpus. Can optionally group utterances and fit vectorizers for each group.
 
-    :param corpus: corpus to fit vectorizers for
-    :param group_models_by: attributes to group utterances by before fitting vectorizers
+    :param corpus: corpus to fit models on
     """
-    utterances = corpus.get_utterances_dataframe()
-    if group_models_by:
-      model_groups = utterances.groupby([self.df_col_name[group] for group in group_models_by])
-      self.mapped_models = model_groups['text'].apply(self.fit_cv).dropna()
-    else:
-      self.mapped_models = pd.Series(self.cv.fit(utterances['text']))
+    model_groups = {}
+    for utt in corpus.iter_utterances():
+      key = self.model_key_selector(utt)
+      if key not in model_groups:
+        model_groups[key] = model_text_selector(utt)
+    self.models = {key: self.fit_cv(text) for key, text in model_groups.items()}
     return self
 
   def fit_cv(self, text):
@@ -99,80 +94,85 @@ class Surprise(Transformer):
 
   def transform(self, corpus: Corpus,
       obj_type: str,
-      group_target_by: List[str] = [],
-      context_selector: Callable[[pd.Series, Tuple], np.ndarray]=lambda s, t: np.ones(len(s)).astype(bool), 
-      model_selector: Callable[[Tuple], Union[str, int]]=lambda _: 0,
-      selector: Callable[[CorpusComponent], bool]=lambda _: True,
-      smooth: bool=True):
+      group_and_models: Callable[[Utterance], Tuple[str, List[str]]]=None,
+      selector: Callable[[CorpusComponent], bool]=lambda _: True):
     """
     Annotates `obj_type` components in `corpus` with surprise scores. Should be called after fit().
 
     :param corpus: corpus to compute surprise for.
     :param obj_type: the type of corpus components to annotate.
-    :param group_target_by: list of attributes to group target text tokens. 
-        i.e. utterance, speaker, conversation. 
-    :param context_selector: function to select context tokens from pandas series to compare a target to.
-        function should return boolean mask over input series.
-    :param model_selector: function to select model (created in `fit` method) to use for a target-context pair.
-        function should return a string or int corresponding to the index of the model in `self.mapped_models`.
+    :param group_and_models: 
     :param selector: function to select objects to annotate. if function returns true, object will be annotated.
-    :param smooth: whether to use laplace smoothing in surprise calculation.
     """
-    utterances = corpus.get_utterances_dataframe()
-    if group_target_by:
-      grouped_utterances = utterances.groupby([self.df_col_name[group] for group in group_target_by])['text'].apply(lambda x: self.cv.build_analyzer()(' '.join(x)))
+    if obj_type == 'corpus':
+      utt_groups = defaultdict(list)
+      group_models = defaultdict(list)
+      for utt in corpus.iter_utterances():
+        if group_and_models:
+          group_name, models = group_and_models(utt)
+        else:
+          group_name = self.model_key_selector(utt)
+          models = [group_name]
+        utt_groups[group_name].append(utt.text)
+        group_models[group_name] += models
+      surprise_scores = {}
+      for group_name in utt_groups:
+        for model_key in group_models[group_name]:
+          surprise_scores[Surprise.format_attr_key(group_name, model_key)] = self.compute_surprise(self.models[model_key], utt_groups[group_name])
+      corpus.add_meta(self.surprise_attr_name, surprise_scores)
+    elif obj_type == 'utterance':
+      for utt in corpus.iter_utterances(selector=selector):
+        if group_and_models:
+          group_name, models = group_and_models(utt)
+          surprise_scores = {}
+          for model_key in models:
+            surprise_scores[Surprise.format_attr_key(group_name, model_key)] = self.compute_surprise(self.models[model_key], utt.text)
+          utt.add_meta(self.surprise_attr_name, surprise_scores)
+        else:
+          group_name = self.model_key_selector(utt)
+          utt.add_meta(self.surprise_attr_name, self.compute_surprise(self.models[group_name], utt.text))
     else:
-      grouped_utterances = pd.Series(self.cv.build_analyzer()(' '.join(utterances['text'])))
-    surprise_scores = {}
-    for idx, (ind, target) in enumerate(grouped_utterances.items()):
-      if (self.verbosity > 0) and (idx % self.verbosity == 0) and (idx > 0):
-        print(idx, '/', len(grouped_utterances))
-      model_ind = model_selector(ind)
-      if model_ind in self.mapped_models:
-        model = self.mapped_models[model_ind]
-        context = list(chain(*grouped_utterances[context_selector(grouped_utterances, ind)]))
-        surprise_scores[ind] = self.compute_surprise(model, target, context, smooth)
-      else:
-        surprise_scores[ind] = np.nan
-    for obj in corpus.iter_objs(obj_type):
-      if selector(obj):
-        ind = tuple(map(lambda x: Surprise.get_obj_field(obj, x), grouped_utterances.index.names))
-        if len(ind) == 1:
-          ind = ind[0]
-        obj.add_meta(self.surprise_attr_name, surprise_scores[ind])
+      for obj in corpus.iter_objs(obj_type, selector=selector):
+        utt_groups = defaultdict(list)
+        group_models = defaultdict(list)
+        for utt in obj.iter_utterances():
+          if group_and_models:
+            group_name, models = group_and_models(utt)
+          else:
+            group_name = self.model_key_selector(utt)
+            models = [group_name]
+          utt_groups[group_name].append(utt.text)
+          group_models[group_name] += models
+        surprise_scores = {}
+        for group_name in utt_groups:
+          for model_key in group_models[group_name]:
+            assert (model_key in self.models), 'invalid model key'
+            if not self.models[model_key]: continue
+            surprise_scores[Surprise.format_attr_key(group_name, model_key)] = self.compute_surprise(self.models[model_key], utt_groups[group_name])
+        obj.add_meta(self.surprise_attr_name, surprise_scores)
     return corpus
 
-  def compute_surprise(self, model: CountVectorizer, target: List[str], context: List[str], smooth):
+  def compute_surprise(self, model: CountVectorizer, target: List[str]):
     """
     :param model: the CountVectorizer to use for finding term-doc matrices
     :param target: a list of tokens in the target
     :param context: a list of tokens in the context
-    :param smooth: whether to use laplace smoothing for cross entropy calculation
     """
-    target_samples = self.sampling_fn(np.array(target), self.target_sample_size, self.n_samples)
-    context_samples = self.sampling_fn(np.array(context), self.context_sample_size, self.n_samples)
+    model_vocab, model_vocab_freq = list(map(np.array, zip(*model.vocabulary_.items())))
+    model_vocab_prob = model_vocab_freq / np.sum(model_vocab_freq)
+    target_tokens = model.build_analyzer()(' '.join(target))
+    target_samples = self.sampling_fn(np.array(target_tokens), self.target_sample_size, self.n_samples)
+    context_samples = self.sampling_fn(model_vocab, self.context_sample_size, self.n_samples, p=model_vocab_prob)
     if target_samples is None or context_samples is None:
       return np.nan
     sample_entropies = np.empty(self.n_samples)
     for i in range(self.n_samples):
       target_doc_terms = np.asarray(model.transform(target_samples[i]).sum(axis=0)).squeeze()
       context_doc_terms = np.asarray(model.transform(context_samples[i]).sum(axis=0)).squeeze()
-      sample_entropies[i] = _cross_entropy(target_doc_terms, context_doc_terms, smooth)
+      sample_entropies[i] = _cross_entropy(target_doc_terms, context_doc_terms, self.smooth)
     return np.nanmean(sample_entropies)
 
   @staticmethod
-  def get_obj_field(obj, field):
-    if type(obj) == Conversation:
-      if field == 'conversation_id':
-        return obj.id
-    elif type(obj) == Speaker:
-      if field == 'speaker':
-        return obj.id
-    else:
-      if field == 'id':
-        return obj.id
-      if field == 'conversation_id':
-        return obj.get_conversation().id
-      if field == 'speaker':
-        return obj.get_speaker().id
+  def format_attr_key(group_name, model_key):
+    return 'GROUP_{}__MODEL_{}'.format(group_name, model_key)
     
